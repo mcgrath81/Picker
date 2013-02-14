b2b60f5bee046db7874760eb39f16405948ca34e
admin <admin@admins-MacBook-4.local>
2013-02-14 10:37:27 +0000

    Initial Commit

---
+++ Picker/PickerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PickerAppDelegate.h
+//  Picker
+//
+//  Created by admin on 14/02/2013.
+//  Copyright (c) 2013 emuse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PickerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Picker/PickerViewController.m
@@ -0,0 +1,29 @@
+//
+//  PickerViewController.m
+//  Picker
+//
+//  Created by admin on 14/02/2013.
+//  Copyright (c) 2013 emuse. All rights reserved.
+//
+
+#import "PickerViewController.h"
+
+@interface PickerViewController ()
+
+@end
+
+@implementation PickerViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
